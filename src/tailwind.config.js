df5ad11774bf6d45002ca2aceb3f6e4292748136
geraldlika97 <82274578+geraldlika97@users.noreply.github.com>
2025-06-04 16:48:08 +0200

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "soul-bg": "#FAF9F6",
+        "soul-gray": "#E0E0E0",
+        "soul-dark": "#4A4A4A",
+        "soul-button": "#8B8B8B",
+      },
+    },
+  },
+  plugins: [],
+};
